7e1b3af46c0b4c6f43166cb60ac4d8502b64fed1
tatp-yf <1305116641@qq.com>
2026-03-08 01:59:04 +0800

    refactor: extract EmpiricalNormalization to common module

--- unilab/algos/torch/fast_sac/learner.py
+++ unilab/algos/torch/fast_sac/learner.py
@@ -17,6 +17,8 @@ import torch.nn.functional as F
 import torch.optim as optim
 from typing import Dict, Tuple
 
+from unilab.algos.torch.common.normalization import EmpiricalNormalization
+
 
 # ---------------------------------------------------------------------------
 # Actor Network (holosoma-style: SiLU + LayerNorm + Tanh squashing)
@@ -561,58 +563,6 @@ class FastSACLearner:
         self.q_optimizer.load_state_dict(state_dict["q_optimizer"])
         self.alpha_optimizer.load_state_dict(state_dict["alpha_optimizer"])
         self.update_count = state_dict.get("update_count", 0)
-
-class EmpiricalNormalization(nn.Module):
-    """Normalize mean and variance of observations using running statistics."""
-
-    def __init__(self, shape, device, eps=1e-2):
-        super().__init__()
-        self.eps = eps
-        self.device = device
-        self.register_buffer("_mean", torch.zeros(shape).unsqueeze(0).to(device))
-        self.register_buffer("_var", torch.ones(shape).unsqueeze(0).to(device))
-        self.register_buffer("_std", torch.ones(shape).unsqueeze(0).to(device))
-        self.register_buffer("count", torch.tensor(0, dtype=torch.long).to(device))
-
-    @property
-    def mean(self):
-        return self._mean.squeeze(0).clone()
-
-    @property
-    def std(self):
-        return self._std.squeeze(0).clone()
-
-    @torch.no_grad()
-    def forward(
-        self, x: torch.Tensor, center: bool = True, update: bool = True
-    ) -> torch.Tensor:
-        if self.training and update:
-            self.update(x)
-        if center:
-            return (x - self._mean) / (self._std + self.eps)
-        else:
-            return x / (self._std + self.eps)
-
-    def update(self, x):
-        batch_size = x.shape[0]
-        batch_mean = torch.mean(x, dim=0, keepdim=True)
-        batch_var = torch.var(x, dim=0, keepdim=True, unbiased=False)
-
-        new_count = self.count + batch_size
-
-        # Welford's online algorithm
-        delta = batch_mean - self._mean
-        self._mean.copy_(self._mean + delta * (batch_size / new_count))
-        delta2 = batch_mean - self._mean
-        m_a = self._var * self.count
-        m_b = batch_var * batch_size
-        M2 = m_a + m_b + delta2.pow(2) * (self.count * batch_size / new_count)
-        self._var.copy_(M2 / new_count)
-        self._std.copy_(self._var.sqrt())
-        self.count.copy_(new_count)
-
-    def inverse(self, y):
-        return y * (self._std + self.eps) + self._mean
 
 
 # ---------------------------------------------------------------------------
