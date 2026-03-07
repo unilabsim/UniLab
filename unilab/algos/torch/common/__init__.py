ceb9d160c647cfa42ade659f29ceb1afb275a39a
tatp-yf <1305116641@qq.com>
2026-03-08 02:01:36 +0800

    refactor: add unified numerical stability utilities

--- unilab/algos/torch/common/__init__.py
+++ unilab/algos/torch/common/__init__.py
@@ -2,5 +2,13 @@
 
 from unilab.algos.torch.common.normalization import EmpiricalNormalization
 from unilab.algos.torch.common.networks import DistributionalQNetwork, Critic
+from unilab.algos.torch.common.stability import check_nan_loss, clip_gradients, safe_tensor
 
-__all__ = ["EmpiricalNormalization", "DistributionalQNetwork", "Critic"]
+__all__ = [
+    "EmpiricalNormalization",
+    "DistributionalQNetwork",
+    "Critic",
+    "check_nan_loss",
+    "clip_gradients",
+    "safe_tensor",
+]
