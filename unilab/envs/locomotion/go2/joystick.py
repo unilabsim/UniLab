fix: use qpos for base height in reward calculation

unilab/envs/locomotion/go2/joystick.py
@@ -141,7 +141,7 @@ class Go2WalkTask(Go2BaseEnv):
         return np.sum(np.square(gyro[:, :2]), axis=1)
 
     def _reward_base_height(self, info: dict) -> np.ndarray:
-        base_height = self._backend.get_body_pos(self._cfg.asset.body_name)[:, 2]
+        base_height = self._backend.get_qpos()[:, 2]
         return np.square(base_height - self._cfg.reward_config.base_height_target)
 
     def _reward_action_rate(self, info: dict) -> np.ndarray: