8b82ca28a39f40fc1b9bde7dd6dd24bfb6544ca8
Dixuan jiang <dixuan.jiang@motphys.com>
2026-03-16 14:24:33 +0800

    support asymmetrical actor-critic

--- unilab/envs/locomotion/g1/joystick.py
+++ unilab/envs/locomotion/g1/joystick.py
@@ -16,7 +16,20 @@ from unilab.base.np_env import NpEnvState
 from unilab.envs.locomotion.g1.base import G1BaseCfg, G1BaseEnv
 from unilab.utils.math_utils import np_quat_mul, np_yaw_to_quat
 
-
+@dataclass
+class obs_cfg:
+    obs_dict = {
+        "vel": 3,
+        "gyro": 3,
+        "gravity": 3,
+        "diff": 12,
+        "dof_vel": 12,
+        "action": 12,
+        "cmd": 3,
+        "phase": 2,
+    }  # 'obs_name': dim
+    actor_obs = ['gyro', 'gravity', 'diff', 'dof_vel', 'action', 'cmd', 'phase']
+    
 @dataclass
 class InitState:
     pos = [0.0, 0.0, 0.754]
@@ -96,6 +109,7 @@ class G1JoystickPPOCfg(G1BaseCfg):
     init_state: InitState = field(default_factory=InitState)
     commands: Commands = field(default_factory=Commands)
     reward_config: RewardConfigPPO = field(default_factory=RewardConfigPPO)
+    obs_config: obs_cfg = field(default_factory=obs_cfg)
 
 
 @registry.env("G1JoystickFlatTerrain", sim_backend="mujoco")
@@ -134,10 +148,30 @@ class G1JoystickPPO(G1BaseEnv):
         }
 
     def _init_obs_space(self):
-        num_obs = 3 + 3 + 3 + self._num_action + self._num_action + self._num_action + 3 + 2
+        num_obs = 0
+        for value in self._cfg.obs_config.obs_dict.values():
+            num_obs += value
         self._observation_space = gym.spaces.Box(
             low=-float("inf"), high=float("inf"), shape=(num_obs,), dtype=float
         )
+        self.actor_indices = self._get_actor_indices()
+    
+    def _get_actor_indices(self):
+        s = 0
+        indices = []
+        for i in self._cfg.obs_config.actor_obs:
+            s = 0
+            for k, v in self._cfg.obs_config.obs_dict.items():
+
+                if k == i:
+                    print(k)
+                    for q in range(s, s+v):
+                        indices.append(q) 
+                    s += v
+                    print(indices)
+                    break
+                s += v
+        return indices
 
     @property
     def observation_space(self) -> gym.spaces.Box:
