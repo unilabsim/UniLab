e1575806a4055b042a1c285780180d0144ce00e2
LeeLeno <1846301824@qq.com>
2026-04-20 10:44:46 +0800

    feat: add G1 whole-body tracking task for FastSAC on MuJoCo

--- src/unilab/envs/motion_tracking/g1/__init__.py
+++ src/unilab/envs/motion_tracking/g1/__init__.py
@@ -2,11 +2,14 @@
 
 from .flip_tracking import G1FlipTrackingCfg, G1FlipTrackingEnv, G1FlipTrackingEnvCfg
 from .tracking import G1MotionTrackingCfg, G1MotionTrackingEnv, G1MotionTrackingEnvCfg
+from .tracking_sac import G1MotionTrackingSACCfg, G1MotionTrackingSACEnv
 
 __all__ = [
     "G1MotionTrackingCfg",
     "G1MotionTrackingEnv",
     "G1MotionTrackingEnvCfg",
+    "G1MotionTrackingSACCfg",
+    "G1MotionTrackingSACEnv",
     "G1FlipTrackingCfg",
     "G1FlipTrackingEnv",
     "G1FlipTrackingEnvCfg",
